c8ac374e499d467e9994da3c999e14f5e8717815
estebanzarate <esteban-zarate@hotmail.com>
2022-05-10 17:21:43 -0300

    update return

--- clase2/funciones_y_closures/app.js
+++ clase2/funciones_y_closures/app.js
@@ -1,15 +1,15 @@
 function mostrarLista(lista) {
-    if(lista) return console.log(lista);
-    if(!lista) return console.log("Lista vacía");
+    if(lista) return lista;
+    if(!lista) return "Lista vacía";
 }
 
-mostrarLista();
+console.log(mostrarLista());
 
-mostrarLista(["dato1", "dato2", "dato3"]);
+console.log(mostrarLista(["dato1", "dato2", "dato3"]));
 
 (function (lista) {
-    if(lista) return console.log(lista);
-    if(!lista) return console.log("Lista vacía");
+    if(lista) lista;
+    if(!lista) return "Lista vacía";
 })([1, 2, 3]);
 
 function crearMultiplicador(num1) {
@@ -19,17 +19,17 @@ function crearMultiplicador(num1) {
 }
 
 function duplicar(num) {
-    return console.log(num ** 2);
+    return num ** 2;
 }
 
 function triplicar(num) {
-    return console.log(num ** 3);
+    return num ** 3;
 }
 
-crearMultiplicador(3);
+console.log(crearMultiplicador(3));
 
-duplicar(crearMultiplicador(3));
-triplicar(crearMultiplicador(3));
+console.log(duplicar(crearMultiplicador(3)));
+console.log(triplicar(crearMultiplicador(3)));
 
-duplicar(crearMultiplicador(2));
-triplicar(crearMultiplicador(2));
+console.log(duplicar(crearMultiplicador(2)));
+console.log(triplicar(crearMultiplicador(2)));
