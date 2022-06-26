14265c17d9c81173b470c3d297058da7cfc27269
estebanzarate <esteban-zarate@hotmail.com>
2022-06-25 21:59:03 -0300

    post cart

--- 14-webpack/primera_entrega_proyecto_final/routes/routerCart.js
+++ 14-webpack/primera_entrega_proyecto_final/routes/routerCart.js
@@ -1,6 +1,14 @@
 import express from "express";
-
 const routerCart = express.Router();
+import Container from '../controllers/container.js';
+
+const cart = new Container('./data/cart.json');
 let admin;
+
+routerCart.post('/', (req, res) => {
+	const products = req.body;
+	cart.save(products);
+	res.json({ message: 'Carrito agregado' });
+});
 
 export default routerCart;
