0b5a7ba80211770c91bcea2c166cce051a0656ac
estebanzarate <esteban-zarate@hotmail.com>
2022-06-25 22:24:59 -0300

    router cart, delete

--- 14-webpack/primera_entrega_proyecto_final/routes/routerCart.js
+++ 14-webpack/primera_entrega_proyecto_final/routes/routerCart.js
@@ -11,4 +11,12 @@ routerCart.post('/', (req, res) => {
 	res.json({ message: 'Carrito agregado' });
 });
 
+routerCart.delete('/:id', (req, res) => {
+	const id = Number(req.params.id);
+	if (isNaN(id)) return res.send({ message: 'Ingresa el ID de un carrito listado' });
+	const cartDeleted = cart.deleteById(id);
+	if (cartDeleted === -1) return res.json({ message: 'El ID no pertenece a un carrito listado' });
+	res.json({ message: 'Carrito eliminado' });
+});
+
 export default routerCart;
