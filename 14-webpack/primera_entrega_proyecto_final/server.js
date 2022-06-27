a3a03e1408bcbb30452b47cd226da2ef75adbb3f
estebanzarate <esteban-zarate@hotmail.com>
2022-06-27 19:21:57 -0300

    path *

--- 14-webpack/primera_entrega_proyecto_final/server.js
+++ 14-webpack/primera_entrega_proyecto_final/server.js
@@ -10,6 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', routerProducts);
 app.use('/api/cart', routerCarts);
+app.use('*', (req, res) => {
+	const path = req.params;
+	const method = req.method;
+	res.send({ error: -2, descripcion: `ruta '${path[0]}' método '${method}' no implementada` });
+});
 
 const server = app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 server.on('error', err => console.log(err));
