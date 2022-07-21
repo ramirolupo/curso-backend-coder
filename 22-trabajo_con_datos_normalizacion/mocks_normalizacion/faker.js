70d33d012883c1bc909fa23ab0d9cf3f4d001905
estebanzarate <esteban-zarate@hotmail.com>
2022-07-21 14:25:22 -0300

    clase22, mocks y normalizacion, randomData function, table

--- 22-trabajo_con_datos_normalizacion/mocks_normalizacion/faker.js
+++ 22-trabajo_con_datos_normalizacion/mocks_normalizacion/faker.js
@@ -1,14 +1,16 @@
 const { faker } = require('@faker-js/faker');
 
-const productsFake = [];
-
-for (let i = 0; i < 5; i++) {
-	const product = {
-		name: faker.name.firstName(),
-		price: faker.commerce.price(0, 500, 0, '$'),
-		img: faker.image.abstract()
+const randomData = () => {
+	const productsFake = [];
+	for (let i = 0; i < 5; i++) {
+		const product = {
+			name: faker.name.firstName(),
+			price: faker.commerce.price(0, 500, 0, '$'),
+			img: faker.image.abstract()
+		}
+		productsFake.push(product);
 	}
-	productsFake.push(product);
+	return productsFake;
 }
 
-module.exports = { productsFake };
+module.exports = randomData;
--- 22-trabajo_con_datos_normalizacion/mocks_normalizacion/server.js
+++ 22-trabajo_con_datos_normalizacion/mocks_normalizacion/server.js
@@ -2,17 +2,12 @@ const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
 const { engine } = require('express-handlebars');
-const Container = require('./container.js');
-const { optionsMariaDB, optionsSQLite3 } = require('./options/config.js');
-const { productsFake } = require('./faker');
+const randomData = require('./faker');
 
 const PORT = 8080;
 const app = express();
 const httpserver = new HttpServer(app);
 const io = new IOServer(httpserver);
-
-const products = new Container(optionsSQLite3, 'products');
-const messages = new Container(optionsMariaDB, 'messages');
 
 app.use(express.static('views'));
 
@@ -20,29 +15,16 @@ app.engine('handlebars', engine());
 app.set('views', './views');
 app.set('view engine', 'handlebars');
 
-app.get('/', async (req, res) => {
-    res.render('form');
-});
-
 app.get('/api/products-test', (req, res) => {
-    res.json(productsFake);
+    res.render('table');
 });
 
 io.on('connection', async socket => {
     console.log('Conexión establecida');
-    const dbProducts = await products.getAll();
-    io.sockets.emit('products', dbProducts);
-    const dbMessages = await messages.getAll();
-    io.sockets.emit('messages', dbMessages);
-    socket.on('product', async product => {
-        products.save(product);
-        const dbProducts = await products.getAll();
-        io.sockets.emit('products', dbProducts);
-    })
-    socket.on('message', async message => {
-        messages.save(message);
-        const dbMessages = await messages.getAll();
-        io.sockets.emit('messages', dbMessages);
+    const data = randomData();
+    io.sockets.emit('products', data);
+    socket.on('product', async data => {
+        io.sockets.emit('products', data);
     })
 });
 
