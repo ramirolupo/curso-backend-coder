51588a9a2570aae0565c60b8cbe549e6481835ca
estebanzarate <esteban-zarate@hotmail.com>
2022-07-24 23:37:45 -0300

    connect mongo

--- 24-cookies_session_storage/log-in_formulario/server.js
+++ 24-cookies_session_storage/log-in_formulario/server.js
@@ -5,7 +5,10 @@ const { engine } = require('express-handlebars');
 const Container = require('./container.js');
 const router = require('./routes/router');
 const { optionsMariaDB, optionsSQLite3 } = require('./options/config.js');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 const PORT = 8080;
 const app = express();
@@ -17,7 +20,14 @@ const messages = new Container(optionsMariaDB, 'messages');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(session({
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://estebanzarate:nERlfPKfZtCRTu0K@cluster0.hggfd.mongodb.net/?retryWrites=true&w=majority',
+        mongoOptions: advancedOptions,
+        ttl: 60,
+        collectionName: 'sessions'
+    }),
     secret: 'secret',
     resave: true,
     saveUninitialized: true,
